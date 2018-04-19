74316424759e0fe488bb04c584a9c5a4d31f3bf4
aNap73 <napolitano.anthony@yahoo.com>
2018-04-19 13:46:42 -0400

    added initial query and output

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var inq = require('inquirer');
 var mysql = require('mysql');
+var linelen = 0;
 require("dotenv").config();
 
 var connection = mysql.createConnection({
@@ -13,18 +14,38 @@ var connection = mysql.createConnection({
 });
 
 function startup() {
- var sql = 'Select * from products';
+ let sql = 'Select * from products';
+ let bHeader = false;
  connection.query(sql,function(err,res){
   if(err){console.log(err); return}
   for (var obj in res){
    let val  = Object.values(res[obj]);
    let key  = Object.keys(res[obj]);
-   console.log('---------------------');
-   for(var i in val){
-     console.log(key[i] + ' : ' + val[i]);
+   let header = '';
+   let line = '';
+   if(!bHeader){
+    header += align(key[0],7) + ' | ';
+    header += align(key[1],40) + ' | ';
+    header += align(key[2],20) + ' | ';
+    header += align(key[3],10) + ' | ';
+    header += align(key[4],10) + ' | ';
+    console.log(header);
+    console.log('---------------------------------------------------------------------------------------------------------');
+    bHeader = true;
    }
+   line += align(val[0],7) + '   ';
+   line += align(val[1],40) + '   ';
+   line += align(val[2],20) + '   ';
+   line += align(val[3],10) + '   ';
+   line += align(val[4],10) + '   ';   
+   console.log(line);
   }
  });
  connection.end();
+}
+function align(val,size){
+  val = String(val).padStart(size/2);
+  val = String(val).padEnd(size);
+ return val;
 }
 startup();
